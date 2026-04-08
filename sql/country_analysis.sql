fa3556c8150c7e25289c9822e83a41d0c929f199
GPrajakta <parijatak43@gmail.com>
2026-04-08 15:10:59 +0530

    Update country_analysis.sql

--- sql/country_analysis.sql
+++ sql/country_analysis.sql
@@ -1,1 +1,21 @@
+-- Top 10 countries by total cases
+SELECT Country_Region, TotalCases
+FROM covid_country_stats
+ORDER BY TotalCases DESC
+LIMIT 10;
 
+-- Top 10 countries by total deaths
+SELECT Country_Region, TotalDeaths
+FROM covid_country_stats
+ORDER BY TotalDeaths DESC
+LIMIT 10;
+
+-- Top 10 countries by total recovered
+SELECT Country_Region, TotalRecovered
+FROM covid_country_stats
+ORDER BY TotalRecovered DESC
+LIMIT 10;
+
+-- Top 10 countries by total recovered
+SELECT Country_Region, TotalRecovered
+FROM covid_country_stats
